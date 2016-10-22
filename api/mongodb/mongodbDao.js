00be0679d110c4a038233f4ab9f02107554ad40e
Patrick McAvoy <patrick.mcavoy@monsanto.com>
2016-10-22 14:08:37 -0500

    Adds in new routes

--- api/mongodb/mongodbDao.js
+++ api/mongodb/mongodbDao.js
@@ -3,7 +3,7 @@
 const mongoDb ={}
 
 
-mongoDb.save = (obj, collectionName, callback)=>{
+mongoDb.save = (obj, collectionName, callback) => {
   const db = global.db
   const collection = db.collection(collectionName)
   collection.insertOne(obj,(err,results)=>{
@@ -13,6 +13,31 @@ mongoDb.save = (obj, collectionName, callback)=>{
     console.log("record insert success"+results)
     callback(err,results)
   })
+}
+
+/* $text fields = {
+*   firstName:
+*   lastName
+*   dob
+*   SSN
+*   location
+*   organization
+* }
+*/
+{}
+mongoDb.getClientBy = (queryParams, collectionName, callback) => {
+  const db = global.db
+  const clientCollection = db.collection(collectionName);
+
+  clientCollection.find(queryParams, (err,results)=>{
+    if(!err){
+      results.toArray(callback);
+    }else {
+      callback(err);
+    }
+
+
+  });
 }
 
 module.exports = mongoDb
--- api/service/routes.js
+++ api/service/routes.js
@@ -1,27 +1,69 @@
+'use strict'
 const router = require('express').Router();
 const mongodbDao = require('../mongodb/mongodbDao')
+const COC_COLLECTION_NAME = "coc";
+const SEEKER_COLLECTION_NAME = "seeker";
 
 
-const handleResponse = (res)=>(err, results)=> {
+const handleResponse = (res) => (err, results) => {
     if (err) {
-        res.error(err);
+      res.error(err);
     } else {
-        res.send(results);
+      console.log('results = ', results);
+      res.status(200);
+      res.send(results);
     }
 }
 
-router.post("/seeker/", (req,res)=>{
+// TODO: iOS Endpoints
+// ** POST - /save **
+router.post("/seeker/save", (req, res) => {
     const body = req.body;
     console.log(body);
-    mongodbDao.save(body,"seeker",handleResponse(res))
-
+    mongodbDao.save(body, SEEKER_COLLECTION_NAME, handleResponse(res));
 })
 
-router.post("/coc/", (req,res)=>{
+// ** GET - /contacts **
+// Query Params
+//  * firstName
+//  * lastName
+//  * dob
+//  * SSN
+//  * location
+//  * organization
+router.get("/seekers", (req, res) => {
+  console.log("Get Seeker Contacts");
+  const { firstName, lastName, dob, SSN, location, organization } = req.query;
+  const seekerQueryParams = {
+    firstName,
+    lastName,
+    dob,
+    SSN,
+    location,
+    organization,
+  };
+
+  mongodbDao.getClientBy(seekerQueryParams, SEEKER_COLLECTION_NAME, handleResponse(res));
+})
+// ** PUT - /update **
+// ** GET - /recommendations **
+router.get("coc/client", (req, res) => {
+  console.log("Get COC Clients");
+  const { SSN } = req.query;
+
+  const clientQueryParams = {
+    SSN,
+  };
+
+  mongodbDao.getClientBy(clientQueryParams, COC_COLLECTION_NAME, handleResponse(res));
+})
+
+// TODO: Web Endpoints
+// ** POST - /coc/client
+router.post("/coc/client", (req, res) => {
     const body = req.body;
     console.log(body);
-    mongodbDao.save(body,"coc",handleResponse(res))
-
+    mongodbDao.save(body, COC_COLLECTION_NAME, handleResponse(res));
 })
 
 module.exports = router
